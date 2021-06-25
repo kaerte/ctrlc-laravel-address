d446b0e58dc0e96e61679e5be6d60e63f5b84c96
Łukasz Kozłowski <lukasz@intercorpmedia.com>
2021-06-25 22:25:13 +0200

    USe loadLaravelMigrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ctrlc\Address\Tests;
 
-use CreateUsersTable;
 use Ctrlc\Address\Providers\AddressesServiceProvider;
 use Ctrlc\Address\Providers\GeoCodingServiceProvider;
 
@@ -13,18 +12,11 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->loadMigrationsFrom(__DIR__.'../database/migrations');
-        $this->loadMigrationsFrom(__DIR__.'../migrations');
+        $this->loadLaravelMigrations();
     }
 
     protected function getPackageProviders($app): array
     {
         return [AddressesServiceProvider::class, GeoCodingServiceProvider::class];
-    }
-
-    protected function getEnvironmentSetUp($app): void
-    {
-        include_once __DIR__.'/migrations/2014_10_12_000000_create_users_table.php';
-        (new CreateUsersTable())->up();
     }
 }
